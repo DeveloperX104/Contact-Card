38f72cf70204cae2cbe346bb2828832f52c0e6b7
Anjolaiya Oladapo(Captain Developer) <87665379+DeveloperX104@users.noreply.github.com>
2022-05-24 15:46:11 +0100

    Initial commit

---
+++ src/contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+  {
+    name: "Anjola",
+    imgURL:
+      "https://media-exp1.licdn.com/dms/image/C4D03AQFT8hud34Wrag/profile-displayphoto-shrink_400_400/0/1652728109732?e=1658966400&v=beta&t=DMqRgZ7l_wug9WHhVioHbgS355eCat-qjCtzqUn7UKQ",
+    phone: "+234 9036933583",
+    email: "dapoanjolaiya@gmail.com"
+  },
+  {
+    name: "Yele ",
+    imgURL:
+      "https://media-exp1.licdn.com/dms/image/C4E03AQGoQKL-jQN8_A/profile-displayphoto-shrink_400_400/0/1549948825969?e=1658966400&v=beta&t=WvdRmbhcJpHwO3f7UbEvwef3mxaOKUN8izXN3N26Cb4",
+    phone: "+234 9078239223",
+    email: "yelebademosi@gmail.com"
+  },
+  {
+    name: "John Paul",
+    imgURL:
+      "https://media-exp1.licdn.com/dms/image/C4D03AQHKr5q-aBhWfQ/profile-displayphoto-shrink_400_400/0/1594591053050?e=1658966400&v=beta&t=dceaxatHlkGEZAatGHWsHHqchHj1dOExnLkpl8xnM2M",
+    phone: "+234 9089433233",
+    email: "jposemene@gmail.com"
+  }
+];
+
+export default contacts;
